e1856b5c0f040638c02d52be8a9d2fe3c69a7942
kannupriya kalra <kannupriyakalra@gmail.com>
2023-09-18 17:55:02 +0100

    Hackerrank: Solved 'FilterArray' and updated TestList.scala

--- src/main/scala/Hackerrank/FilterArray.scala
+++ src/main/scala/Hackerrank/FilterArray.scala
@@ -1,5 +1,21 @@
 package Hackerrank
 
+//https://www.hackerrank.com/challenges/fp-filter-array/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 object FilterArray {
 
+  def f(delim: Int, ls: List[Int]): List[Int] = {
+    ls match {
+      case ::(head, next) => if (head < delim) head :: f(delim, next) else f(delim, next)
+      case Nil => Nil
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    f(3, List(1, 2, 3, 4, 5, 6)).foreach(println)
+    // f(3, List()).foreach(println)
+    //f(7, List()).foreach(println)
+    // f(-1, List()).foreach(println)
+
+  }
 }
