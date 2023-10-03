8323baf6ad6532c3d957406eefa541cf254c82b3
kannupriya kalra <kannupriyakalra@gmail.com>
2023-10-03 12:51:12 +0100

    Hackerrank: AreaUnderCurvesAndVolumeOfRevolvingACurve

---
+++ src/main/scala/Hackerrank/AreaUnderCurvesAndVolumeOfRevolvingACurve.scala
@@ -0,0 +1,6 @@
+package Hackerrank
+
+//https://www.hackerrank.com/challenges/area-under-curves-and-volume-of-revolving-a-curv/problem?isFullScreen=true&h_r=next-challenge&h_v=zen
+object AreaUnderCurvesAndVolumeOfRevolvingACurve {
+
+}
