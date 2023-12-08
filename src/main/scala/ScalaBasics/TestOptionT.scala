4c57c005b5ed934ec9945f42d2b8b1d6532d6771
kannupriya kalra <kannupriyakalra@gmail.com>
2023-12-08 17:02:37 +0000

    TestOptionT

--- src/main/scala/ScalaBasics/TestOptionT.scala
+++ src/main/scala/ScalaBasics/TestOptionT.scala
@@ -10,15 +10,22 @@ object TestOptionT extends App{
   val f11: Future[Int] = f1.map(a => a + 1) //map is used to change the type inside.
   val f22: Future[Option[Int]] = f2.map(a => a.map(x => x + 1))
 
+  // Incrementing Future[Option[Int]] using 2 map seemed complicated which is why OptionT type was created
+
   // Implementation of OptionT
-  //OptionT is a wrapper over Future[Option[A]] with a convenience method map so we can directly access and do computations on A.
+  // OptionT is a wrapper over Future[Option[A]] with a convenience method map so we can directly access and do computations on A.
   case class OptionT[A](value: Future[Option[A]]) {
 
     def map[B](f: A => B): OptionT[B] = OptionT(value.map(a => a.map(f)))
 
   }
 
-  // how to increment f2: Future[Option[Int]]
+  // How to increment f2: Future[Option[Int]]
   val f23: Future[Option[Int]] = OptionT(f2).map(a => a + 1).value
+
+  def abcd[A, B](o1: OptionT[A], f: A => B): OptionT[B] = o1.map(f)
+
+
+
 
 }
