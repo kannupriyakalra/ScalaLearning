e1eeab5704c21f02dc1fa010be3e941a57ab5822
kannupriya kalra <kannupriyakalra@gmail.com>
2023-12-10 12:40:35 +0000

    PascalsTriangle

--- src/main/scala/Hackerrank/PascalsTriangle.scala
+++ src/main/scala/Hackerrank/PascalsTriangle.scala
@@ -3,12 +3,13 @@ package Hackerrank
 //https://www.hackerrank.com/challenges/pascals-triangle/problem?isFullScreen=true
 
 /*
-1
-1 1
-1 2 1
-1 3 3 1
-1 4 6 4 1
-....
+The value at the  row and  column of the triangle is equal to nCr = n!/(r! * (n-r)!)  where indexing starts from 0.
+
+0C0
+1C0 1C1
+2C0 2C1 2C2
+3C0 3C1 3C2 3C3
+4C0 4C1 4C2 4C3 4C4
 
 nCr = n!/(r! * (n-r)!)  ie no. of ways to select r items from a given set of n items.
 3C0 = 3C3 = 1
@@ -22,18 +23,19 @@ nCr = n!/(r! * (n-r)!)  ie no. of ways to select r items from a given set of n i
 4C3 = 4
 4C4 = 1
 
-dynamic programming
+dynamic programming- when previously computed results are used to create new results.
 
-4C2 = 3C2 + 3C1 = 3 + 3 = 6
+4C2 = 3C2 + 3C1 = 3 + 3 = 6 //every element is sum of its top and top left element.
 
 nCr = (n-1)Cr + (n-1)C(r-1) , n- row, r- column
 
-1 3 3 1
-1 4  6  4 1
+1
+1 1
+1 2 1
+1 3 3  1
+1 4 6  4  1
 1 5 10 10 5 1
 1 6 15 20 15 6 1
-
-every element is sum of its top and top left element.
 
  */
 object PascalsTriangle {
@@ -58,10 +60,19 @@ object PascalsTriangle {
     val stdin = scala.io.StdIn
     val k = stdin.readLine.trim.toInt
 
-    val firstRow = Array(1)
-    // println(firstRow.mkString(" "))
+    val firstRow = Array(1) //as constraint is k lies b/w 2 to 10, so we can set first row.
+
+    //time complexity- O(n^2), space complexity- O(n)
+    val (lastRow, result) = (2 to k).foldLeft((firstRow, firstRow.mkString)) { case ((prevRow, result), i) => //can write (lastRow, result) as t@(lastRow, result) means tuple named t looks like (lastRow, result)
+      val nextRow = computeNextRow(prevRow)
+      (nextRow, result + "\n" + nextRow.mkString(" ")) //use StringBuilder instead
+    }
+
+    //println(t._2)
+    println(result)
 
     //   Alternative solution - 1 (not a functional solution as var is used)
+    //    println(firstRow.mkString(" "))
     //    var prevRow = firstRow
     //    for (i <- 2 to k) {
     //      val nextRow = computeNextRow(prevRow)
@@ -70,23 +81,16 @@ object PascalsTriangle {
     //    }
 
     //   Alternative solution - 2 (not a functional solution as println is used repeatedly, println is a side effect so this is not a pure function)
+    //        println(firstRow.mkString(" "))
     //        (2 to k).foldLeft(firstRow) { (prevRow, i) =>
     //          val nextRow = computeNextRow(prevRow)
     //          println(nextRow.mkString(" "))
     //          nextRow
     //        }
-
-    //time complexity- O(n^2), space complexity- O(n)
-    val (lastRow, result) = (2 to k).foldLeft((firstRow, firstRow.mkString)) { case ((prevRow, result), i) =>
-      val nextRow = computeNextRow(prevRow)
-      (nextRow, result + "\n" + nextRow.mkString(" ")) //use StringBuilder instead
-    }
-
-    println(result)
-
   }
 
 }
+
 /*
 time complexity- O(n^2)
 computeNextRow- O(n)
