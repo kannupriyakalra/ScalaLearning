e397bcf5170cab18a5fc21781331249eb723fd18
kannupriya kalra <kannupriyakalra@gmail.com>
2024-01-04 21:41:32 +0000

    HackerrankSolved: StringMingling- analysed time complexity

--- src/main/scala/Hackerrank/StringMingling.scala
+++ src/main/scala/Hackerrank/StringMingling.scala
@@ -1,6 +1,5 @@
 package Hackerrank
 
-import scala.::
 import scala.annotation.tailrec
 
 //https://www.hackerrank.com/challenges/string-mingling/problem?isFullScreen=true
@@ -24,7 +23,6 @@ object StringMingling extends App {
   //  }.mkString)
 
 
-
   //Solution -3
 
   //  def mingledStrings(p: String, q: String): String = {
@@ -44,38 +42,42 @@ object StringMingling extends App {
   //  }
 
 
-
   //Solution -4 Best
 
-    def mingledStrings(p: String, q: String): String = { //time complexity = O(n), space complexity = O(n)
+  def mingledStrings(p: String, q: String): String = { //time complexity = O(n), space complexity = O(n)
 
-      val pList = p.toList
-      val qList = q.toList
+    val pList = p.toList
+    val qList = q.toList
 
-      @tailrec
-      def checker(pList: List[Char], qList: List[Char], result: List[(Char, Char)] = List[(Char, Char)]()): List[(Char, Char)] = {
+    @tailrec
+    def checker(pList: List[Char], qList: List[Char], result: List[(Char, Char)] = List[(Char, Char)]()): List[(Char, Char)] = {
 
-        (pList, qList) match {
-          case (head1 :: tail1, head2 :: tail2) =>  checker(tail1, tail2,(head1, head2) :: result )
-          case (Nil, Nil) => result
-        }
+      (pList, qList) match {
+        case (head1 :: tail1, head2 :: tail2) => checker(tail1, tail2, (head1, head2) :: result)
+        case (Nil, Nil) => result
       }
-
-      checker(pList, qList).map { case (x, y) => s"$x$y" }.reverse.mkString
     }
+
+    checker(pList, qList).map { case (x, y) => s"$x$y" }.reverse.mkString
+  }
 
 
 
   //Solution -5
 
-//    def mingledStrings(p: String, q: String, finalString: String = ""): String = { //time complexity = O(n), space complexity = O(n)
-//      if (p.isEmpty && q.isEmpty) return finalString
-//      mingledStrings(p.tail, q.tail, finalString + s"${p.head}${q.head}")
-//    }
+  //    def mingledStrings(p: String, q: String, finalString: String = ""): String = { //time complexity = O(n), space complexity = O(n)
+  //      if (p.isEmpty && q.isEmpty) return finalString
+  //      mingledStrings(p.tail, q.tail, finalString + s"${p.head}${q.head}") //s"$finalString${p.head}${q.head}"
+  //    }
 
-  //why is it Terminated due to timeout for big i/p when it is tail recursive?-----------------chatgpt couldnt tell
+  //  why is it Terminated due to timeout for big i/p when it is tail recursive?
+  //its because string append operation has o(n) complexity whereas in solution 4 prepend has O(1) complexity and time complexity is also based on operations used
+  //  https://docs.scala-lang.org/overviews/collections/performance-characteristics.html , append is concatenation
+  //Important Conclusion: String concatenation and list concatenation are very time expensive operations with O(n) complexity for just this expression" s"$finalString${p.head}${q.head}" so
+  // overall making it O(n2)?--check? , always avoid them and use cons instead as its complexity is O(1)
 
 
+  //how to find time complexity of solution-5 which has string interpolation?----------------
 
   //Solution -6
 
@@ -89,32 +91,29 @@ object StringMingling extends App {
   //  println(mingledStrings(p.toList, q.toList)) //Time limit exceeded for big input
 
 
-
   //Solution -7
 
-//  val stdin = scala.io.StdIn
-//  val p = stdin.readLine //global variables for solution
-//  val q = stdin.readLine
-//
-//  def mingledStrings(index: Int, finalString: StringBuilder): String = { //time complexity = O(n), space complexity = O(n)
-//    if (index == p.length) return finalString.toString
-//    finalString += p(index)
-//    finalString += q(index)
-//    mingledStrings(index + 1, finalString)
-//  }
-//
-//  println(mingledStrings(0, new StringBuilder()))
-//
-//  //StringBuilder is a mutable collection
+  //  val stdin = scala.io.StdIn
+  //  val p = stdin.readLine //global variables for solution
+  //  val q = stdin.readLine
+  //
+  //  def mingledStrings(index: Int, finalString: StringBuilder): String = { //time complexity = O(n), space complexity = O(n)
+  //    if (index == p.length) return finalString.toString
+  //    finalString += p(index)
+  //    finalString += q(index)
+  //    mingledStrings(index + 1, finalString)
+  //  }
+  //
+  //  println(mingledStrings(0, new StringBuilder()))
+  //
+  //  //StringBuilder is a mutable collection
 
 
+  val stdin = scala.io.StdIn
+  val p = stdin.readLine
+  val q = stdin.readLine
 
-
-      val stdin = scala.io.StdIn
-      val p = stdin.readLine
-      val q = stdin.readLine
-
-   println(mingledStrings(p, q)) //o/p- hraacnkkeerr
+  println(mingledStrings(p, q)) //o/p- hraacnkkeerr
 }
 
 /*
