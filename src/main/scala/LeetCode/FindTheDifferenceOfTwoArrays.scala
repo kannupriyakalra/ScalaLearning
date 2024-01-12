78e897cba806dfaba05199e3857760d4c06e3c5e
kannupriya kalra <kannupriyakalra@gmail.com>
2024-01-12 19:56:26 +0000

    LeetcodeSolved: FindTheDifferenceOfTwoArrays

--- src/main/scala/LeetCode/FindTheDifferenceOfTwoArrays.scala
+++ src/main/scala/LeetCode/FindTheDifferenceOfTwoArrays.scala
@@ -1,5 +1,7 @@
 package LeetCode
 
+import scala.annotation.tailrec
+
 //https://leetcode.com/problems/find-the-difference-of-two-arrays/description/?envType=study-plan-v2&envId=leetcode-75
 object FindTheDifferenceOfTwoArrays extends App {
 
@@ -15,13 +17,13 @@ object FindTheDifferenceOfTwoArrays extends App {
 
   //solution-2
 
-  //  def findDifference(nums1: Array[Int], nums2: Array[Int]): List[List[Int]] = { //time complexity- O(n), space complexity- O(n)
-  //
-  //    val s1 = nums1.toSet
-  //    val s2 = nums2.toSet
-  //
-  //    List(s1.filter(element => !s2.contains(element)).toList, s2.filter(element => !s1.contains(element)).toList)
-  //  }
+//    def findDifference(nums1: Array[Int], nums2: Array[Int]): List[List[Int]] = { //time complexity- O(n), space complexity- O(n)
+//
+//      val s1 = nums1.toSet
+//      val s2 = nums2.toSet
+//
+//      List(s1.filter(element => !s2.contains(element)).toList, s2.filter(element => !s1.contains(element)).toList)
+//    }
 
   //solution-3
 
@@ -38,28 +40,55 @@ object FindTheDifferenceOfTwoArrays extends App {
 
   //solution-4
 
-  def findDifference(nums1: Array[Int], nums2: Array[Int]): List[List[Int]] = { //time complexity- O(n), space complexity- O(n)
+  //  def findDifference(nums1: Array[Int], nums2: Array[Int]): List[List[Int]] = { //time complexity- O(n), space complexity- O(n)
+  //
+  //    var m1: Map[Int, Boolean] = Map()
+  //    for (i <- nums1) {
+  //      m1 = m1.updated(i, true)
+  //    }
+  //
+  //    var m2: Map[Int, Boolean] = Map()
+  //    for (i <- nums2) {
+  //      m2 = m2.updated(i, true)
+  //    }
+  //
+  //    val l1: List[Int] = (for {
+  //      k <- m1.keys
+  //      if !m2.contains(k)
+  //    } yield k).toList
+  //
+  //    val l2: List[Int] = (for {
+  //      k <- m2.keys
+  //      if !m1.contains(k)
+  //    } yield k).toList //The yield keyword is used to collect these transformed values into a new iterator and then the combined values of for loop are assigned to l2 after transforming into a list.
+  //
+  //    List(l1, l2)
+  //
+  //  }
 
+  //solution-5
+  def findDifference(nums1: Array[Int], nums2: Array[Int]): List[List[Int]] = {
 
-    var m1: Map[Int, Boolean] = Map()
-    for (i <- nums1) {
-      m1 = m1.updated(i, true)
+    @tailrec
+    def findUniqueElements(set1: Set[Int], set2: Set[Int], acc: List[Int]): List[Int] = {
+      if (set1.isEmpty) acc
+      else {
+        if (!set2.contains(set1.head)) findUniqueElements(set1.tail, set2, set1.head :: acc)
+        else findUniqueElements(set1.tail, set2, acc)
+      }
     }
 
-    var m2: Map[Int, Boolean] = Map()
-    for (i <- nums1) {
-      m2 = m2.updated(i, true)
-    }
+    val s1 = nums1.toSet
+    val s2 = nums2.toSet
 
-    //tbc
+    val l1 = findUniqueElements(s1, s2, Nil).reverse
+    val l2 = findUniqueElements(s2, s1, Nil).reverse
 
+    List(l1, l2)
   }
+
 
   println(findDifference(Array(1, 2, 3), Array(2, 4, 6)))
 
 
 }
-
-/*
-
- */
