d84a0952cab10c6bc2de3986df261e6ce59fc9bf
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-06-24 09:02:12 +0100

    ScalaBasics: Implemented filter on an Option

--- src/main/scala/ScalaBasics/TestFilter.scala
+++ src/main/scala/ScalaBasics/TestFilter.scala
@@ -3,18 +3,30 @@ package ScalaBasics
 object TestFilter extends App {
 
   // Create a filter on an Option
-  def filterOption[A](input: Option[A], f: A => Boolean): Option[A] = {
-    input match {
-      case Some(value) => if (f(value)) Some(value) else None
+  def filterOption[A](option: Option[A], predicate: A => Boolean): Option[A] = {
+    option match {
+      case Some(value) => if (predicate(value)) Some(value) else None
       case None => None
     }
   }
 
-  val o: Option[Int] = Some(10)
-  val m: Option[Int] = None
-  val fxn: Int => Boolean = (input: Int) => input % 2 == 0
+  val someOption: Option[Int] = Some(10)
+  val noneOption: Option[Int] = None
+  val isEven: Int => Boolean = (number: Int) => number % 2 == 0
 
-  println(filterOption(o, fxn)) // Output: Some(10)
-  println(filterOption(m, fxn)) // Output: None
+  println(filterOption(someOption, isEven)) // Output: Some(10)
+  println(filterOption(noneOption, isEven)) // Output: None
 
+  // Implement filter on a List
+  def filterList[A](list: List[A], predicate: A => Boolean): List[A] = {
+    list match {
+      case head :: tail => if (predicate(head)) head :: filterList(tail, predicate) else filterList(tail, predicate)
+      case Nil => Nil
+    }
+  }
+
+  val numbers: List[Int] = List(1, 2, 3, 4, 5)
+  val greaterThanTwo: Int => Boolean = (number: Int) => number > 2
+
+  println(filterList(numbers, greaterThanTwo)) // Output: List(3, 4, 5)
 }
