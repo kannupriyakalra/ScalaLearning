79229809b664d7374751d7f53906a55bab6bc401
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-06-25 15:30:53 +0100

    ScalaBasics: Make a generic filter that works on both list and option

--- src/main/scala/ScalaBasics/TestFilter.scala
+++ src/main/scala/ScalaBasics/TestFilter.scala
@@ -29,4 +29,28 @@ object TestFilter extends App {
   val greaterThanTwo: Int => Boolean = (number: Int) => number > 2
 
   println(filterList(numbers, greaterThanTwo)) // Output: List(3, 4, 5)
+
+  // Typeclasses
+  // Make a generic filter that works on both list and option
+  // Higher Kinded Types, F[_] is another type parameter that we have introduced and its a higher kinded type. List, Option above is replaced by F.
+  trait Filterable[F[_]]{
+    def filterElements[A](input: F[A], predicate: A => Boolean): F[A]
+
+  }
+
+  implicit val listFilterable: Filterable[List] = new Filterable[List] {
+    override def filterElements[A](input: List[A], predicate: A => Boolean): List[A] = input.filter(predicate)
+  }
+
+  implicit val optionFilterable: Filterable[Option] = new Filterable[Option] {
+    override def filterElements[A](input: Option[A], predicate: A => Boolean): Option[A] = input.filter(predicate)
+  }
+
+  def genericFilter[F[_], A](input: F[A], predicate: A => Boolean)(implicit filterable: Filterable[F]): F[A] =
+    filterable.filterElements(input, predicate)
+
+  println(genericFilter(someOption, isEven))
+  println(genericFilter(numbers, greaterThanTwo))
+
 }
+
