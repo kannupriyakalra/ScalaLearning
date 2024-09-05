3baf25f1dc082ec5a6c5e1085c1829268d002475
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-09-05 13:07:23 +0100

    TourOfScala: ScalaRange

--- src/main/scala/TourOfScala/ScalaRange.scala
+++ src/main/scala/TourOfScala/ScalaRange.scala
@@ -24,6 +24,31 @@ object ScalaRange extends App {
   val expected2: Int = 7
   assert(result2 == expected2, result2)
 
+  (0 to 50).toList
+
   println("Congratulations ! 'Happiness is when what you think, what you say, and what you do are in harmony.' - Mahatma Ghandi")
 
 }
+
+/*
+scala repl
+
+scala> (0 to 50)
+val res1: scala.collection.immutable.Range.Inclusive = Range 0 to 50
+
+scala> (0 to 50).toList
+val res2: List[Int] = List(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50)
+
+scala> (0 to 50).toSet
+val res3: Set[Int] = HashSet(0, 5, 10, 14, 1, 6, 9, 2, 8, 4, 42, 24, 37, 25, 20, 46, 29, 21, 33, 28, 38, 13, 41, 45, 17, 32, 34, 22, 44, 27, 12, 49, 7, 39, 3, 35, 48, 18, 50, 16, 31, 11, 43, 40, 26, 23, 36, 30, 19, 47, 15)
+
+scala> (0 to 50).toSet + 1 //adding a element, which doesn't add if it exist already
+val res4: Set[Int] = HashSet(0, 5, 10, 14, 1, 6, 9, 2, 8, 4, 42, 24, 37, 25, 20, 46, 29, 21, 33, 28, 38, 13, 41, 45, 17, 32, 34, 22, 44, 27, 12, 49, 7, 39, 3, 35, 48, 18, 50, 16, 31, 11, 43, 40, 26, 23, 36, 30, 19, 47, 15)
+
+scala> (0 to 50).toSet + 51
+val res5: Set[Int] = HashSet(0, 5, 10, 14, 1, 6, 9, 2, 8, 4, 42, 24, 37, 25, 20, 46, 29, 21, 33, 28, 38, 13, 41, 45, 17, 32, 34, 22, 44, 27, 12, 49, 7, 39, 3, 35, 48, 18, 50, 16, 31, 11, 43, 40, 26, 23, 36, 30, 51, 19, 47, 15)
+
+scala> (0 to 50).toSet - 1
+val res6: Set[Int] = HashSet(0, 5, 10, 14, 6, 9, 2, 8, 4, 42, 24, 37, 25, 20, 46, 29, 21, 33, 28, 38, 13, 41, 45, 17, 32, 34, 22, 44, 27, 12, 49, 7, 39, 3, 35, 48, 18, 50, 16, 31, 11, 43, 40, 26, 23, 36, 30, 19, 47, 15)
+
+ */
