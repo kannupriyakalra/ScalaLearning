7f6fc793103ae1cf41d3d27ae65dee889b1f6bce
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-09-05 10:57:13 +0100

    Revision TestTry

--- src/main/scala/ScalaBasics/TestTry.scala
+++ src/main/scala/ScalaBasics/TestTry.scala
@@ -1,15 +1,35 @@
 package ScalaBasics
 
+/*
+The Try type represents a computation that may either result in an exception, or return a successfully computed value. Instances of Try[T], are either an instance of Success[T] or Failure[T].
+For example, Try can be used to perform division on a user-defined input, without the need to do explicit exception-handling in all of the places that an exception might occur.
+
+Go through Try.scala class.
+
+flatMap- Returns the given function applied to the value from this `Success` or returns this if this is a `Failure`.
+def flatMap[U](f: T => Try[U]): Try[U]
+
+map- Maps the given function to the value from this `Success` or returns this if this is a `Failure`.
+def map[U](f: T => U): Try[U]
+
+final case class Success[+T](value: T) extends Try[T]
+final case class Failure[+T](exception: Throwable) extends Try[T]
+
+Errors are treated as data, they are not thrown. Exceptions are not thrown in scala, they are made into a object and sent to failure. This is why scala is safe language.
+ */
+
+import scala.annotation.tailrec
 import scala.io.StdIn
 import scala.util.{Failure, Success, Try}
 
 object TestTry extends App{
 
+  @tailrec
   def divide: Try[Int] = {
     val dividend: Try[Int] = Try(StdIn.readLine("Enter an Int that you'd like to divide:\n").toInt)
     val divisor: Try[Int] = Try(StdIn.readLine("Enter an Int that you'd like to divide by:\n").toInt)
-    val problem: Try[Int] = dividend.flatMap(x => divisor.map(y => x/y))  //as you have to divide 2 try[Int] so do it via flatmap n map to extract value of x n y, divisor.map(10/0) --returns failure
-    // 2nd approach is via pattern match divdend match 2 cases success n failure n in success divisor match 2 cases success n failure
+    val problem: Try[Int] = dividend.flatMap(x => divisor.map(y => x/y))  //as you have to divide 2 try[Int] so do it via flatmap n map to be able to extract value of x and y,
+    // divisor.map(10/0) --returns failure, 2nd approach is via pattern match, dividend match 2 cases success n failure and in success divisor match 2 cases success n failure
     problem match {
       case Success(v) =>
         println("Result of " + dividend.get + "/"+ divisor.get +" is: " + v)
@@ -17,11 +37,12 @@ object TestTry extends App{
       case Failure(e) =>
         println("You must've divided by zero or entered something that's not an Int. Try again!")
         println("Info from the exception: " + e.getMessage)
-        divide //if you put in wrong i/p then call divide again so you are given a second chance incase you entered wrong i/p.
+        divide //if you put in wrong i/p then call divide again so you are given a second chance.
     }
   }
 
   divide
+
   /*
   o/p- Enter an Int that you'd like to divide:
 10
@@ -49,21 +70,3 @@ Enter an Int that you'd like to divide:
    */
 
 }
-
-/*
-The Try type represents a computation that may either result in an exception, or return a successfully computed value. Instances of Try[T], are either an instance of Success[T] or Failure[T].
-For example, Try can be used to perform division on a user-defined input, without the need to do explicit exception-handling in all of the places that an exception might occur.
-
-Go through Try.scala class.
-
-flatMap- Returns the given function applied to the value from this `Success` or returns this if this is a `Failure`.
-def flatMap[U](f: T => Try[U]): Try[U]
-
-map- Maps the given function to the value from this `Success` or returns this if this is a `Failure`.
-def map[U](f: T => U): Try[U]
-
-final case class Success[+T](value: T) extends Try[T]
-final case class Failure[+T](exception: Throwable) extends Try[T]
-
-Errors are treated as data, they are not thrown. Exceptions are not thrown in scala, they are made into a object and sent to failure. This is why scala is safe language.
- */
