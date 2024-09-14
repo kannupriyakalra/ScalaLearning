c1bca8726efd08515a49502fdc722ed6e915d72c
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-09-14 17:08:19 +0100

    TourOfScala: ScalaImplicitClass

--- src/main/scala/TourOfScala/ScalaImplicitClass.scala
+++ src/main/scala/TourOfScala/ScalaImplicitClass.scala
@@ -28,6 +28,8 @@ implicit class NAME(val value_name: TypeToOverride) extends AnyVal {
 The second form will be more efficient, it will not create a new instance. The first form will create a new instance of the Type which can be inefficient if overused.
 Then, why not use the AnyVal form all the time ? There are a multitude of reasons, I could go over each and every ones of them, but this Stackoverflow
 answer (https://stackoverflow.com/questions/14929422/should-implicit-classes-always-extend-anyval/14931302#14931302) explains it in a lot of details.
+
+When "extends AnyVal" is used, new object in heap memory is not created.
  */
 object ScalaImplicitClass extends App{
 
