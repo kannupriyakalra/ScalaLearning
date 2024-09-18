dd4e41aac12095ac18f048d70298b831c8295def
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-09-18 16:29:18 +0100

    TourOfScala: ScalaAbstractClass

--- src/main/scala/TourOfScala/ScalaAbstractClass.scala
+++ src/main/scala/TourOfScala/ScalaAbstractClass.scala
@@ -17,24 +17,33 @@ To practice the concepts of Object Oriented Programming, Try to access the field
  */
 object ScalaAbstractClass extends App {
 
-  abstract class Shape(//elements of class are by default private, for a case class they are by default public.
+  abstract class Shape( //elements of class are by default private, for a case class they are by default public.
                         name: String,
                         protected val lengthOfSides: Int,
                         numberOfSides: Int
                       ) {
     def circumference: Double = numberOfSides * lengthOfSides
 
+    def area: Double
+
     override def toString: String =
       s"$name shape of size $lengthOfSides " +
-        f"with $numberOfSides sides have a circumference of $circumference%1.2f"
+        f"with $numberOfSides sides have a circumference of $circumference%1.2f" +
+        f" with area of $area%1.2f"
   }
 
-  case class Square(size: Int) extends Shape("Square", size, 4)
+  case class Square(size: Int) extends Shape("Square", size, 4) {
+    override def area: Double = size * size
+  }
 
-  case class Triangle(size: Int) extends Shape("Triangle", size, 3)
+  case class Triangle(size: Int) extends Shape("Triangle", size, 3) {
+    override def area: Double = (Math.sqrt(3) / 4) * size * size
+  }
 
   case class Circle(radius: Int) extends Shape("Circle", radius, 0) {
-    override lazy val circumference: Double = scala.math.Pi * 2.0 * lengthOfSides //def has no arguments ie why overriden with val.
+    override lazy val circumference: Double = scala.math.Pi * 2.0 * lengthOfSides //def has no arguments ie why overriden with val. , 2pie r is circumference
+
+    override def area: Double = scala.math.Pi * radius * radius
   }
 
   val square: Square = Square(size = 4)
@@ -52,6 +61,10 @@ object ScalaAbstractClass extends App {
   assert(square.circumference == 16)
   assert(triangle.circumference == 9)
   assert(Math.abs(circle.circumference - 12) <= 1)
+
+  assert(square.area == 16)
+  assert(triangle.area - 3 <= 1)
+  assert(Math.abs(circle.area - 12) <= 1)
 
   println(
     "Congratulations ! 'Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better.' - Samuel Beckett"
