f26f2e9062634a3dcfc30094417802c1b4a399cb
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-11-22 19:20:35 +0000

    ScalaBasics: TestMonoid - monoid for user defined types

--- src/main/scala/ScalaBasics/TestMonoid.scala
+++ src/main/scala/ScalaBasics/TestMonoid.scala
@@ -22,13 +22,14 @@ object Monoid {
     override def combine(first: String, second: String): String = first + second
   }
 
-  implicit def setGenericMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
+  //used def instead of val as we need type parameter [A], val means an object in memory which is of a specific type so it can't be of generic type.
+  implicit def setMonoid[A]: Monoid[Set[A]] = new Monoid[Set[A]] {
     override def empty: Set[A] = Set()
 
     override def combine(first: Set[A], second: Set[A]): Set[A] = first ++ second
   }
 
-  implicit def listGenericMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
+  implicit def listMonoid[A]: Monoid[List[A]] = new Monoid[List[A]] {
     override def empty: List[A] = Nil
 
     override def combine(first: List[A], second: List[A]): List[A] = first ++ second
@@ -39,7 +40,7 @@ object Monoid {
 object TestMonoid extends App {
 
   //reduce here is an example of adhoc polymorphism as reduce function can be used with different behaviours which are addition and multiplication monoid.
-  def reduce[A](input: List[A])(implicit monoid: Monoid[A]): A =
+  def reduce[A](input: List[A])(implicit monoid: Monoid[A]): A = //comprehend this as List[A] is reduced to A and monoid sent implicitly is of type A using the combine behaviour from monoid.
     input.foldLeft(monoid.empty)(monoid.combine)
 
   println(reduce(List(2, 2, 2, 2))(Monoid.additionIntMonoid)) // monoid object is passed explicitly here to avoid forward reference
@@ -56,6 +57,7 @@ object TestMonoid extends App {
   //companion object and used additionIntMonoid instead.
   println(reduce(List("2", "2", "2", "2")))
 
+  //monoid for user defined types:
   case class Score(i: Int)
 
   case object Score {
@@ -66,11 +68,12 @@ object TestMonoid extends App {
     }
   }
 
-  println(reduce(List(Score(2), Score(3), Score(4), Score(5))))
+  println(reduce(List(Score(2), Score(3), Score(4), Score(5)))) //o/p- Score(14)
 
-  println(reduce(List(List(1, 2), List(3), List(4), List(5))))
-  println(reduce(List(List("1", "2"), List("3"), List("4"), List("5"))))
-  println(reduce(List(List(Score(2), Score(3)), List(Score(4), Score(5))))) //o/p- List(Score(2), Score(3), Score(4), Score(5))
+  println(reduce(List(List(1, 2), List(3), List(4), List(5)))) //o/p- List(1, 2, 3, 4, 5)
+  println(reduce(List(List("1", "2"), List("3"), List("4"), List("5")))) //o/p- List(1, 2, 3, 4, 5)
+  println(reduce(List(List(Score(2), Score(3)), List(Score(4), Score(5))))) //o/p- List(Score(2), Score(3), Score(4), Score(5)) //as type A in reduce is List[Score] ie why the o/p is
+  // of type A ie List[Score] as per definition of reduce method.
   println(reduce(reduce(List(List(Score(2), Score(3)), List(Score(4), Score(5)))))) //o/p- Score(14)
 
 
@@ -90,9 +93,14 @@ object TestMonoid extends App {
 
   }
 
-  println(reduce(List(Runs(2), Runs(3), Runs(4), Runs(5))))
+  println(reduce(List(Runs(2), Runs(3), Runs(4), Runs(5)))) //Type A = Runs[Int], for Monoid[Runs[Int]] we need Monoid[Int] implicitly.
   println(reduce(List(Runs("2"), Runs("3"), Runs("4"), Runs("5"))))
 
+  println(reduce(List(Runs(Runs(2)), Runs(Runs(3)))))
+  //for runsMonoid function if input is of type Monoid[Int] => Monoid[Runs[Int]],
+  //if input is of type Monoid[Runs[Int]] => Monoid[Runs[Runs[Int]]]
+
+  println(reduce(List(Runs(Runs(Runs(2))), Runs(Runs(Runs(3))))))
 
 }
 
