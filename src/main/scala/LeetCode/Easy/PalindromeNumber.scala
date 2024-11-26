be41cacd85e02e3f58d76bb084fd63980d1b50d9
Kannupriya Kalra <39022252+kannupriyakalra@users.noreply.github.com>
2024-11-26 15:30:38 +0000

    LeetCode2/1000: PalindromeNumber

---
+++ src/main/scala/LeetCode/Easy/PalindromeNumber.scala
@@ -0,0 +1,37 @@
+package LeetCode.Easy
+
+// https://leetcode.com/problems/palindrome-number/description/
+object PalindromeNumber extends App {
+
+  /**
+   * For i in 0 to length/2:
+   * if x[i] == x[x.length-1-i] true and continue
+   * else false
+   */
+
+  // Time complexity- O(n), Space Complexity- O(n); n = np. of digits in the integer
+  def isPalindrome(x: Int): Boolean = {
+
+    val str: String = x.toString
+    val length = str.length
+
+    (0 to length / 2).forall(i => str(i) == str(length - 1 - i))
+
+  }
+
+  // Time complexity- O(n), Space Complexity- O(n)
+    def isPalindrome2(x: Int): Boolean = {
+
+      val charArray = x.toString
+      val reverseCharArray = charArray.reverse
+
+      charArray == reverseCharArray
+
+    }
+
+  println(isPalindrome(121))
+  println(isPalindrome(-121))
+  println(isPalindrome(10))
+
+}
+
