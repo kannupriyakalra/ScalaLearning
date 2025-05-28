1c030d6ac5f0a9f55ad2d45388937feedf41c908
kannupriyakalra <kannupriyakalra@gmail.com>
2025-05-28 06:19:16 +0530

    Hackerrank: HelloWorldNTimes

--- src/main/scala/Hackerrank/HelloWorldNTimes.scala
+++ src/main/scala/Hackerrank/HelloWorldNTimes.scala
@@ -3,6 +3,7 @@ package Hackerrank
 //https://www.hackerrank.com/challenges/fp-hello-world-n-times/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 object HelloWorldNTimes extends App {
 
+  // Solution- 1: Recursive solution - best solution
   def f(n: Int): Unit = {
     if (n > 0) {
       println("Hello World")
@@ -36,3 +37,5 @@ object HelloWorldNTimes extends App {
   f4(n)
 
 }
+
+//println is not a pure function, it has a side effect of printing to the console. So code containing it is not part of pure functional programming paradigm.
